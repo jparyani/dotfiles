fd48d651efdab644af04bfd61ea8d369cecb010f
Jason Paryani <github@jparyani.com>
2014-06-30 16:54:59 -0700

    Fix brew outdated script

--- bin/brew-email-outdated.sh
+++ bin/brew-email-outdated.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-/usr/local/bin/brew outdated | /usr/local/bin/python /Users/jason/email_me.py - 'mars: brew outdated' 'mars-outdated@jparyani.com'
+/usr/local/bin/brew update
+( /usr/local/bin/brew outdated ; /usr/local/bin/brew cask doutdated ) | /usr/local/bin/python /Users/jason/email_me.py - 'mars: brew outdated' 'mars-outdated@jparyani.com'
